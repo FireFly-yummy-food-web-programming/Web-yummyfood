05ffca30a9788c909c1fd3894fb6e4cbffbb3b4e
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-03-17 15:00:39 +0700

    feat/layouts: css the layout again. admin header sections.

---
+++ resources/views/admin/contents/dashboard.blade.php
@@ -0,0 +1,17 @@
+<div class="row d-flex justify-content-md-center dashboard-content" style="margin-top: 110px;">
+    <div class="col-md-3 dashboard-content-user bg-primary rounded-3 p-3 m-3" style="width: 30%">
+        <h4>Number of users</h4>
+        <h5>1234</h5>
+        <p>Increased by 30%</p>
+    </div>
+    <div class="col-md-3 ashboard-content-total-income bg-success rounded-3 p-3 m-3" style="width: 30%">
+        <h4>Weakly orders</h5>
+        <h5>1234</h5>
+        <p>Decreased by 10%</p>
+    </div>
+    <div class="col-md-3 dashboard-content-total-income bg-info rounded-3 p-3 m-3" style="width: 30%">
+        <h4>Revenue</h4>
+        <h5>1234</h5>
+        <p>Increased by 30%</p>
+    </div>
+</div>
--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.admin')
 @section('content')
-    <div ></div>
+    @include('admin.contents.dashboard')
 @endsection
