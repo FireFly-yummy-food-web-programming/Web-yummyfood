33f3ac7dbdf338aeb13a0493dce2df81b2d3af52
Duong Thi Hong Lam <honglamhk2004@gmail.com>
2024-03-19 09:11:01 +0700

    feat/dashboard: Code for the dish management page interface, displaying dish data from data

---
+++ app/Models/Dish.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Dish extends Model
+{
+    use HasFactory;
+    protected $table = 'Dish';
+    public function getAllDish(){
+        $dishList = DB::select('SELECT dish.*, category.category_name
+        FROM dish
+        INNER JOIN category
+        ON dish.category_id = category.category_id;');
+        // dd($dishList);
+        return $dishList;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\DishController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,4 +31,8 @@ Route::prefix('users')->group(function () {
 Route::prefix('admin')->group(function () {
     //Contacts
     Route::get('/', [AdminController::class, 'getContacts'])->name('contact');
+    // Route::get('/manage-oders', [OrdersController::class,'get'])->name('manage-oders');
+    Route::get('/manage-dish', [DishController::class,'getDish'])->name('manage-dish');
+    // Route::get('/manage-users', [UsersController::class,'getContacts'])->name('manage-users');
+    // Route::get('/manage-banner', [BannerController::class,'getContacts'])->name('manage-banner');
 });
