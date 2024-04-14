20e68784adcf82280ebf01a085bacf022a0ebfd3
VanThao98 <144594456+VanThao98@users.noreply.github.com>
2024-04-14 16:26:48 +0700

    Feature/banner (#24)
    * change div banner
    
    * finish get and display banners
    
    * fex: change code from develop
    
    ---------
    
    Co-authored-by: Van Thao <levanthao@gmail.com>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,6 +16,11 @@ class HomeController extends Controller
     $listRandom = DB::table('dish')->inRandomOrder()->limit(10)->get();
     $listRandomPromotion = DB::table('dish')->where('discount', '>', 1)->inRandomOrder()->limit(10)->get(); 
     $listDishId = session()->get('favoriteId', []);
+    // get banner
+    $bigBanner = DB::table('banners')->get();
+    //get category
+    $category =DB::table('category')->get();
+
     if (session()->has('favoriteNewId')) {
         $favoriteNewId = session()->get('favoriteNewId');
         if (!in_array($favoriteNewId, $listDishId)) {
@@ -23,7 +28,7 @@ class HomeController extends Controller
             session()->put('favoriteId', $listDishId); 
         }
     }
-    return view('clients.home', compact('listDish', 'allDish', 'listRandom', 'listRandomPromotion', 'listDishId'));
+    return view('clients.home', compact('listDish', 'allDish', 'listRandom', 'listRandomPromotion', 'listDishId','bigBanner','category'));
 }
     public function isFavorite($user_id, $dish_id){
         $isFavorite = DB::table('favorites')
