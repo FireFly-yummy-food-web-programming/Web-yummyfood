773fd47b6f34dee4d402bc9b1e58ea726bf32d85
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-04-17 19:42:12 +0700

    feat/recommentDish: finish recomment disk

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,40 +4,69 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Dish;
+use App\Models\User;
+use App\Models\Orders;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use App\Models\Favorite;
+
 class HomeController extends Controller
 {
-    public function index(Request $request)
-{
-    $listDish = DB::table('dish')->limit(10)->get();
-    $allDish = DB::table('dish')->get();
-    $listRandom = DB::table('dish')->inRandomOrder()->limit(10)->get();
-    $listRandomPromotion = DB::table('dish')->where('discount', '>', 1)->inRandomOrder()->limit(10)->get(); 
-    $listDishId = session()->get('favoriteId', []);
-    // get banner
-    $bigBanner = DB::table('banners')->get();
-    //get category
-    $category =DB::table('category')->get();
 
-    if (session()->has('favoriteNewId')) {
-        $favoriteNewId = session()->get('favoriteNewId');
-        if (!in_array($favoriteNewId, $listDishId)) {
-            $listDishId[] = $favoriteNewId; 
-            session()->put('favoriteId', $listDishId); 
+    public function index(Request $request)
+    {
+        // $listDish = DB::table('dish')->limit(10)->get();
+        $user_id = $request->session()->get('user_id');
+        $listDish = [];
+
+        if ($user_id) {
+            // Lấy đơn hàng của người dùng
+            $user_orders = DB::table('orders')
+                ->join('users', 'orders.user_id', '=', 'users.user_id')
+                ->join('order_detail', 'order_detail.order_id', '=', 'orders.order_id')
+                ->select('orders.*', 'users.user_id', 'users.Username', 'users.Email', 'order_detail.*')
+                ->where('orders.user_id', $user_id)
+                ->get();
+
+            $purchasedProductIds = [];
+            foreach ($user_orders as $order) {
+                $purchasedProductIds[] = $order->dish_id;
+            }
+
+            $listDish = Dish::whereNotIn('dish_id', $purchasedProductIds)
+                ->inRandomOrder() // Lấy ngẫu nhiên
+                ->take(10) // Giới hạn số lượng sản phẩm đề xuất
+                ->get();
+        } else {
+            $listDish = DB::table('dish')->limit(10)->get();
         }
+
+        $allDish = DB::table('dish')->get();
+        $listRandom = DB::table('dish')->inRandomOrder()->limit(10)->get();
+        $listRandomPromotion = DB::table('dish')->where('discount', '>', 1)->inRandomOrder()->limit(10)->get();
+        $listDishId = session()->get('favoriteId', []);
+        // get banner
+        $bigBanner = DB::table('banners')->get();
+        //get category
+        $category = DB::table('category')->get();
+
+        if (session()->has('favoriteNewId')) {
+            $favoriteNewId = session()->get('favoriteNewId');
+            if (!in_array($favoriteNewId, $listDishId)) {
+                $listDishId[] = $favoriteNewId;
+                session()->put('favoriteId', $listDishId);
+            }
+        }
+        return view('clients.home', compact('category', 'listDish', 'allDish', 'listRandom', 'listRandomPromotion', 'listDishId', 'bigBanner'));
     }
-    return view('clients.home', compact('listDish', 'allDish', 'listRandom', 'listRandomPromotion', 'listDishId','bigBanner','category'));
-}
-    public function isFavorite($user_id, $dish_id){
+    public function isFavorite($user_id, $dish_id)
+    {
         $isFavorite = DB::table('favorites')
-                        ->where('user_id', $user_id)
-                        ->where('dish_id', $dish_id)
-                        ->exists();
+            ->where('user_id', $user_id)
+            ->where('dish_id', $dish_id)
+            ->exists();
         return $isFavorite;
-    
     }
     // function add to cart
     public function cart()
@@ -47,12 +76,12 @@ class HomeController extends Controller
     public function addToCart($id)
     {
         $dish = Dish::findOrFail($id);
- 
+
         $cart = session()->get('cart', []);
- 
-        if(isset($cart[$id])) {
+
+        if (isset($cart[$id])) {
             $cart[$id]['quantity']++;
-        }  else {
+        } else {
             $cart[$id] = [
                 "dish_name" => $dish->dish_name,
                 "image_dish" => $dish->image_dish,
@@ -62,30 +91,30 @@ class HomeController extends Controller
                 "user_id" => session()->get('user_id)')
             ];
         }
- 
+
         session()->put('cart', $cart);
         return redirect()->back()->with('success', 'Product add to cart successfully!');
     }
- 
+
     public function update(Request $request)
     {
-        if($request->id && $request->quantity){
+        if ($request->id && $request->quantity) {
             $cart = session()->get('cart');
             $cart[$request->id]["quantity"] = $request->quantity;
             session()->put('cart', $cart);
             session()->flash('success', 'Cart successfully updated!');
         }
     }
- 
+
     public function remove(Request $request)
     {
-        if($request->id) {
+        if ($request->id) {
             $cart = session()->get('cart');
-            if(isset($cart[$request->id])) {
+            if (isset($cart[$request->id])) {
                 unset($cart[$request->id]);
                 session()->put('cart', $cart);
             }
             session()->flash('success', 'Product successfully removed!');
         }
-    }    
+    }
 }
