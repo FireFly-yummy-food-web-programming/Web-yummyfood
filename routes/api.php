cdbb2d9e1d9ed147fbb34dd5e0ee5efdf6450c0c
ThuyNgan015 <ngan.ho25@student.passerellesnumeriques.org>
2024-07-15 09:41:45 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BuyerController;
+use App\Http\Controllers\AuctionsController;
+use App\Http\Controllers\DepositController;
+use App\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('buyer')->group(function () {
+    Route::post('register', [BuyerController::class, 'register']);
+    Route::put('update-profile', [BuyerController::class, 'updateProfile']);
+    Route::delete('delete-profile', [BuyerController::class, 'deleteProfile']);
+    Route::put('forgot-password', [BuyerController::class, 'forgotPassword']);
+    Route::get('auctions', [AuctionsController::class, 'getAuctions']);
+    Route::get('auction-history', [AuctionsController::class, 'getAuctionHistory']);
+    Route::post('place-deposit', [DepositController::class, 'placeDeposit']);
+    Route::post('make-payment', [PaymentController::class, 'makePayment']);
 });
